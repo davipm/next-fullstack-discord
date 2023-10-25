67e3e255047a00214d0f0b484c536f4d559a42ff
Davi Pereira <davi.p.m94@gmail.com>
2023-10-25 01:35:41 -0300

    update socket

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -3,3 +3,24 @@ import { NextApiRequest } from "next";
 import { Server as ServerIO } from "socket.io";
 
 import { NextApiResponseServerIo } from "@/types";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  if (!res.socket.server.io) {
+    const path = "/api/socket/io";
+    const httpServer: NetServer = res.socket.server as any;
+    res.socket.server.io = new ServerIO(httpServer, {
+      path: path,
+      addTrailingSlash: false,
+    });
+  }
+
+  res.end();
+};
+
+export default ioHandler;
