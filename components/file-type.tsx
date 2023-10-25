70b406f2f8a41f4896388093280ce07e8107b6a1
Davi Pereira <davi.p.m94@gmail.com>
2023-10-25 00:58:10 -0300

    adding file upload

--- components/file-type.tsx
+++ components/file-type.tsx
@@ -4,47 +4,50 @@ import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-type HandleIfPDFProps = {
+type FileTypeProps = {
   value: string;
   onChange: (url?: string) => void;
   fileType: string;
 };
 
-const FileType = ({ onChange, value, fileType }: HandleIfPDFProps) => (
-  <section
-    className={
-      fileType === "pdf"
-        ? "relative flex items-center p-2 mt-2 rounded-md bg-background/10"
-        : "relative h-20 w-20"
-    }
-  >
-    {fileType === "pdf" ? (
-      <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-    ) : (
-      <Image src={value} alt="Upload" className="rounded-full" />
-    )}
+const FileType = ({ onChange, value, fileType }: FileTypeProps) => {
+  const containerClassName =
+    fileType === "pdf"
+      ? "relative flex items-center p-2 mt-2 rounded-md bg-background/10"
+      : "relative h-20 w-20";
+  const closeBtnClassName =
+    fileType === "pdf"
+      ? "bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+      : "top-0 right-0";
 
-    {fileType === "pdf" && (
-      <a
-        href={value}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+  return (
+    <section className={containerClassName}>
+      {fileType === "pdf" ? (
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+      ) : (
+        <Image src={value} alt="Upload" className="rounded-full" />
+      )}
+
+      {fileType === "pdf" && (
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+      )}
+
+      <button
+        onClick={() => onChange("")}
+        className={closeBtnClassName}
+        type="button"
       >
-        {value}
-      </a>
-    )}
-
-    <button
-      onClick={() => onChange("")}
-      className={`bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm ${
-        fileType === "pdf" ? "-top-2 -right-2" : "top-0 right-0"
-      }`}
-      type="button"
-    >
-      <X className="h-4 w-4" />
-    </button>
-  </section>
-);
+        <X className="h-4 w-4" />
+      </button>
+    </section>
+  );
+};
 
 export default FileType;
