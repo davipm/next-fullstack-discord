64671b15c82f9d527e7e84e30cfba482c8f66f2c
Davi Pereira <davi.p.m94@gmail.com>
2023-11-05 13:50:53 -0300

    adding chat welcome

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,35 +1,44 @@
 import { MemberRole } from "@prisma/client";
 import { NextRequest, NextResponse as res } from "next/server";
 import { v4 as uuidv4 } from "uuid";
+import * as z from "zod";
 
 import currentProfile from "@/lib/current-profile";
 import prisma from "@/lib/db";
 
+const postSchema = z.object({
+  name: z.string(),
+  imageUrl: z.string().url(),
+});
+
 export async function POST(req: NextRequest) {
-  try {
-    const profile = await currentProfile();
-    const { name, imageUrl } = await req.json();
+  const profile = await currentProfile();
+  const body = await req.json();
+  const response = postSchema.safeParse(body);
 
-    if (!profile) return res.json({ message: "Unauthorized" }, { status: 401 });
+  if (!profile) return res.json({ message: "Unauthorized" }, { status: 401 });
 
-    const server = await prisma.server.create({
-      data: {
-        profileId: profile.id,
-        name,
-        imageUrl,
-        inviteCode: uuidv4(),
-        channels: {
-          create: [{ name: "general", profileId: profile.id }],
-        },
-        members: {
-          create: [{ profileId: profile.id, role: MemberRole.ADMIN }],
-        },
-      },
-    });
-
-    return res.json(server, { status: 200 });
-  } catch (error) {
-    console.log("[CHANNEL_ID_PATCH]", error);
-    return res.error();
+  if (!response.success) {
+    const { errors } = response.error;
+    return res.json({ message: "Invalid request", errors }, { status: 400 });
   }
+
+  const { name, imageUrl } = response.data;
+
+  const server = await prisma.server.create({
+    data: {
+      profileId: profile.id,
+      name,
+      imageUrl,
+      inviteCode: uuidv4(),
+      channels: {
+        create: [{ name: "general", profileId: profile.id }],
+      },
+      members: {
+        create: [{ profileId: profile.id, role: MemberRole.ADMIN }],
+      },
+    },
+  });
+
+  return res.json(server, { status: 200 });
 }
