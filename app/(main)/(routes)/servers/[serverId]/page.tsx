e4e88c627d09eb89b9863d95fad4d873c33e138b
Davi Pereira <davi.p.m94@gmail.com>
2023-11-11 00:07:39 -0300

    adding server id page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,42 @@
-export default function Page() {
-  return (
-    <div>
-      <p></p>
-    </div>
-  );
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import currentProfile from "@/lib/current-profile";
+import prisma from "@/lib/db";
+
+interface Props {
+  params: { serverId: string };
+}
+
+export default async function Page({ params }: Props) {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await prisma.server.findFirst({
+    where: {
+      inviteCode: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/server/${params.serverId}/channels/${initialChannel.id}`);
 }
