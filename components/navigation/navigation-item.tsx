ae4c2f77ddf9d84cf709799a599fe0266b6955e9
Davi Pereira <davi.p.m94@gmail.com>
2023-11-13 14:12:07 -0300

    adding navigation item

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+"use client";
 
-// import { Container } from "./styles";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
 
-export default function NavigationItem() {
-  const [item, setItem] = useState(null);
+import ActionTooltip from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+export default function NavigationItem({ id, name, imageUrl }: Props) {
+  const params = useParams() as { serverId: string | null };
+  const router = useRouter();
 
   return (
-    <div>
-      <p></p>
-    </div>
+    <ActionTooltip side="right" label={name} align="center">
+      <button
+        type="button"
+        onClick={() => router.push(`/server/${id}`)}
+        className="group relative flex items-center"
+      >
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]",
+          )}
+        />
+
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]",
+          )}
+        >
+          <Image fill src={imageUrl} alt={name} />
+        </div>
+      </button>
+    </ActionTooltip>
   );
 }
