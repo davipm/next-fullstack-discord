47ba04f39ef26f3722cd79a8fbd04a4f9987b9fe
Davi Pereira <davi.p.m94@gmail.com>
2023-11-13 21:36:24 -0300

    update MediaRoom

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,4 +1,12 @@
-import { useState } from "react";
+"use client";
+
+import "@livekit/components-styles";
+
+import { useUser } from "@clerk/nextjs";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
 
 interface Props {
   chatId: string;
@@ -7,11 +15,49 @@ interface Props {
 }
 
 export default function MediaRoom({ chatId, video, audio }: Props) {
-  const [item, setItem] = useState(null);
+  const { user } = useUser();
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const name = `${user?.firstName} ${user?.lastName}`;
+
+      const { data } = await axios.get<{ token: string }>(
+        `/api/livekit?room=${chatId}&username=${name}`,
+      );
+
+      return data.token;
+    },
+    enabled: !!user?.firstName || !!user?.lastName,
+  });
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-sm text-zinc-500 dark:text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <p className="text-sm text-zinc-500 dark:text-zinc-400">Error!</p>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <p>MediaRoom</p>
-    </div>
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={data}
+      video={video}
+      audio={audio}
+      connect
+    >
+      <VideoConference />
+    </LiveKitRoom>
   );
 }
