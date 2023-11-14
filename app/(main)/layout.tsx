32b53d42571a4e38e427b41e97bc92088308db12
Davi Pereira <davi.p.m94@gmail.com>
2023-11-14 02:45:36 -0300

    update layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,10 +3,10 @@ import { PropsChildren } from "@/types";
 
 export default function Layout({ children }: PropsChildren) {
   return (
-    <section className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+    <section className="h-screen">
+      <aside className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar />
-      </div>
+      </aside>
       <main className="md:pl-[72px] h-full">{children}</main>
     </section>
   );
