7fa6cd28290cd1a0224005f9203da07d898086e6
Davi Pereira <davi.p.m94@gmail.com>
2023-11-13 21:40:17 -0300

    update img

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -42,6 +42,7 @@ export default function NavigationItem({ id, name, imageUrl }: Props) {
             fill
             src={imageUrl}
             alt={name}
+            className="object-cover"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
